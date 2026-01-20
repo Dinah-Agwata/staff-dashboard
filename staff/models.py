c03d1ae5c2fbc80f2703a14950c06f8c1e310f82
Dinah-Agwata <dinahbagwata@gmail.com>
2026-01-20 07:21:49 +0300

    Add status field to user model

--- staff/models.py
+++ staff/models.py
@@ -15,15 +15,21 @@ class User(AbstractUser):
         ("rn","RN")
     ]
 
-    role=models.CharField(max_length=20, choices=ROLE_CHOICES)
-    organization=models.ForeignKey(Organization,on_delete=models.CASCADE, null=True, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    organization = models.ForeignKey(Organization,on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=20, default="active",
+     choices=[
+         ("active","Active"),
+         ("inactive","Inactive"),
+    ],
+    )
 
-def __str__(self):
+    def __str__(self):
         return self.username
 
 class Event(models.Model):
-    staff=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events")
-    created_at=models.DateTimeField(auto_now_add=True)
+    staff = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.staff}-{self.created_at}"
